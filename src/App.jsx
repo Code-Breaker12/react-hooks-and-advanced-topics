33f8f4adad8ac4104a9b7a969b696d0af1e6c193
Akanksha Khandelwal <akanksha@growpital.com>
2024-01-18 00:34:00 +0530

    rendering list and clearing list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+import { data } from './data';
+// import { people } from './data';
 
 function App() {
+  // const people = ['prateek', 'prateeksha', 'raghav', 'akanksha' , 'gargi', 'madhav'];
+  const [baseData, setData] = useState(data);
+  const removeItem = (id) => {
+    console.log('hey', id, data);
+    const updatedData = baseData?.filter(item => item.id !== id);
+    setData(updatedData);
+  };
   return (
     <>
-     
+    {baseData?.map(({id , name})=>
+    <ul key={id}>
+      <li>{name}</li>
+      <button onClick={()=>removeItem(id)}>Click me</button>
+      </ul>)}
     </>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const data = [
+    {id: 1, name: 'john'},
+    {id: 2, name: 'joh'},
+    {id: 3, name: 'jo'},
+    {id: 4, name: 'j'},
+];
+
+export const people = [
+    {id: 1, name: 'bob', nickName: 'StudMuffin'},
+    {id: 1, name: 'peter'},
+    {id: 1, name: 'oliver', images: [
+        {
+            small: {
+                url: 'https...........'
+            }
+        },
+        {
+            small: {
+                url: 'https...........'
+            }
+        }
+    ]},
+
+]
